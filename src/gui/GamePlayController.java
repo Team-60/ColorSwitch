80869c9bcad8cb919504bf57137d2fff5c686398
Divyansh Rastogi <divyansh19464@iiitd.ac.in>
2020-11-22 16:49:19 +0530

    Added Pause Functionality

--- src/gui/GamePlayController.java
+++ src/gui/GamePlayController.java
@@ -2,17 +2,20 @@ package gui;
 
 import gameEngine.Game;
 import gameEngine.GamePlay;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 public class GamePlayController {
     private GamePlay gamePlay;
+    private Boolean paused;
     @FXML
     private Button button;
     public void init(GamePlay _gamePlay) {
         this.gamePlay = _gamePlay;
         this.button.setStyle("-fx-background-radius: 200px; -fx-background-color: #808588");
+        this.paused = false;
     }
     @FXML
     public void pauseHoverActive() {
@@ -29,6 +32,18 @@ public class GamePlayController {
     @FXML
     public void pausePressed() { // most probably have to serialize here to pause, focus shifts to button (IMP)
         System.out.println("Pause Pressed Gameplay");
-
+        AnimationTimer animationTimer = this.gamePlay.getAnimationTimer();
+        if (this.paused) {
+            System.out.println("paused again");
+            this.paused = false;
+            this.gamePlay.getCanvas().requestFocus();
+            animationTimer.start();
+        }
+        else {
+            System.out.println("play again");
+            this.paused = true;
+            GamePlay.PreviousFrameTime = -1;
+            animationTimer.stop();
+        }
     }
 }
